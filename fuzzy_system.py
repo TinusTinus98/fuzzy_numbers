2472c03333d27c540cdf318b1c65a0577b14a797
Martin <martin.lecaille@hotmail.com>
2022-10-07 16:46:40 +0200

    [+] modification of fuzzy system

--- fuzzy_system.py
+++ fuzzy_system.py
@@ -1,12 +1,13 @@
 import numpy as np
 from pyearth import Earth
+from scipy.stats import spearmanr
 
 
 class FuzzyMetric:
     def __init__(self, X) -> None:
         self.m = X.shape[1]  # number of dimensions
         self.n = X.shape[0]  # number of observations
-        self.i = 0
+        self.l = 0
         self.X = X
         self.x_star = np.array([0.5, 0.4, 0.5, 0.6, 0.7, 0.4, 0.5, 0.2, 0.3, 0.6])
         self.k = [np.ones((self.m, self.n))]
@@ -14,17 +15,26 @@ class FuzzyMetric:
         self.cfi_calculation()
 
     def cfi_calculation(self):  # Composite fuzzy indicator
-        assert len(self.cfi_list) != self.i
-        k_matrix = np.array([self.k[self.i] for _ in range(self.n)])
-        x_star_matrix = np.array([self.x_star[self.i] for _ in range(self.n)])
+        assert len(self.cfi_list) != self.l
+        k_matrix = np.array([self.k[self.l] for _ in range(self.n)])
+        x_star_matrix = np.array([self.x_star[self.l] for _ in range(self.n)])
         distance_list = np.abs(x_star_matrix - self.X)
         metric_list = np.divide(k_matrix, k_matrix + distance_list)
         cfi = np.prod(metric_list, axis=0)
         self.cfi_list.append(cfi)
 
     def mars_calculation(self):
-        assert len(self.cfi_list) != self.i
+        assert len(self.cfi_list) != self.l
         model = Earth()
-        model.fit(self.X, self.cfi_calculation[self.i])  # Fit an Earth model
+        model.fit(self.X, self.cfi_calculation[self.l])  # Fit an Earth model
         print(model.trace())  # Print the model
         print(model.summary())
+
+    def correlation(self):
+        coef, p = spearmanr(self.cfi_list[self.l], self.cfi_list[self.l - 1])
+        print("Spearmans correlation coefficient: %.3f" % coef)
+        alpha = 0.05
+        if p <= alpha:  # interpret the significance
+            print("Samples are correlated (reject H0) p=%.3f" % p)
+
+    # def gamma_correlation(self):
